5a52076ebbcb7ac353b9ad823136c9738408d17b
MALANKA96 <voksa.kolya@gmail.com>
2022-01-05 19:59:21 +0300

    add save replay clear

--- js/script.js
+++ js/script.js
@@ -1,38 +1,125 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+
+let line_width = document.getElementById("line_width");
+let line_color = document.getElementById("line_color");
+let btn_save = document.getElementById("btn_save");
+let btn_replay = document.getElementById("btn_replay");
+let btn_clear = document.getElementById("btn_clear");
+
 let isMouseDown = false;
+let coordinates = [];
+let coordinateX = 0;
+let coordinateY = 0;
 
 canvas.width = window.innerWidth - 30;
 canvas.height = window.innerHeight - 150;
 
-canvas.addEventListener("mousedown", function () {
+canvas.addEventListener("mousedown", function (e) {
   isMouseDown = true;
+
+  coordinateX = e.layerX;
+  coordinateY = e.layerY;
+
+  ctx.fillStyle = String(line_color.value);
+  ctx.strokeStyle = String(line_color.value);
+
+  coordinates.push([coordinateX, coordinateY]);
+  ctx.lineWidth = line_width.value * 2;
+
+  ctx.beginPath();
+  ctx.arc(coordinateX, coordinateY, line_width.value, 0, Math.PI * 2);
+  ctx.fill();
 });
 
 canvas.addEventListener("mouseup", function () {
   isMouseDown = false;
   ctx.beginPath();
+  coordinates.push("mouseup");
 });
 
 canvas.addEventListener("mousemove", function (e) {
   if (isMouseDown) {
-    let line_width = document.getElementById("line_width").value;
-    let line_color = document.getElementById("line_color").value;
-    
-    ctx.fillStyle = String(line_color);
-    ctx.strokeStyle = String(line_color);
+    coordinateX = e.layerX;
+    coordinateY = e.layerY;
 
-    ctx.lineWidth = document.getElementById("line_width").value * 2;
-    console.log(ctx.lineWidth);
+    coordinates.push([coordinateX, coordinateY]);
 
-    ctx.lineTo(e.clientX - 7.5, e.clientY - 126);
+    ctx.fillStyle = String(line_color.value);
+    ctx.strokeStyle = String(line_color.value);
+
+    ctx.lineWidth = line_width.value * 2;
+
+    ctx.lineTo(coordinateX, coordinateY);
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.arc(e.clientX - 7.5, e.clientY - 126, line_width, 0, Math.PI * 2);
+    ctx.arc(coordinateX, coordinateY, line_width.value, 0, Math.PI * 2);
     ctx.fill();
 
     ctx.beginPath();
-    ctx.moveTo(e.clientX - 7.5, e.clientY - 126);
+    ctx.moveTo(coordinateX, coordinateY);
   }
+});
+
+function save() {
+  localStorage.setItem("coordinates", JSON.stringify(coordinates));
+  console.log("btn_save");
+}
+
+function replay() {
+  coordinates = JSON.parse(localStorage.getItem("coordinates"));
+  let timer = setInterval(function () {
+    if (!coordinates) {
+      clearInterval(timer);
+      ctx.beginPath();
+      return;
+    }
+
+    let crd = coordinates.shift();
+    if (!coordinates.length) {
+      clearInterval(timer);
+    }
+    e = {
+      layerX: crd["0"],
+      layerY: crd["1"],
+    };
+
+    coordinateX = e.layerX;
+    coordinateY = e.layerY;
+
+    ctx.lineWidth = line_width.value * 2;
+
+    ctx.lineTo(coordinateX, coordinateY);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(coordinateX, coordinateY, line_width.value, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(coordinateX, coordinateY);
+  }, 1);
+  console.log("btn_replay");
+}
+
+function clear() {
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+
+  console.log("btn_clear");
+}
+
+btn_save.addEventListener("click", () => {
+  save();
+});
+btn_replay.addEventListener("click", () => {
+  clear();
+  replay();
+});
+btn_clear.addEventListener("click", () => {
+  clear();
 });
